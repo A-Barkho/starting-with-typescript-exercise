384f159016f6c56d0ca05f65868aec09a2c287ad
A-Barkho <azadbarkho@gmail.com>
2021-04-13 08:28:52 +0200

    ask uName with prompt

--- hello-world.js
+++ hello-world.js
@@ -2,3 +2,10 @@ function doIt() {
     var theText = "Hello World";
     alert(theText);
 }
+function uName() {
+    var fName;
+    var lName;
+    fName = prompt("Vorname!");
+    lName = prompt("Nachname!");
+    document.getElementById("firstHeader").innerHTML = ("Hallo " + fName + " " + lName);
+}
--- hello-world.ts
+++ hello-world.ts
@@ -1,6 +1,12 @@
 
 function doIt(): void {
     const theText: string = "Hello World";
-  
     alert(theText);
+}
+function uName(): void {
+    var fName:string;
+    var lName:string;
+    fName=prompt("Vorname!");
+    lName=prompt("Nachname!");
+    document.getElementById("firstHeader").innerHTML=("Hallo "+fName+" "+lName);
 }
